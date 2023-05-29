e190e001adf593c9e3adaf896c273860c7376d92
andreadae <100606105+andreadae@users.noreply.github.com>
2023-05-29 13:16:45 +0200

    change hello world text

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express(); 
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course :-)'); 
+    res.send('Hoi Andrea, das funktioniert ja wie am Schnürchen :-)'); 
 });
 
 app.listen(PORT, HOST); 
